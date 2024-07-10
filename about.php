8f18b59d17c31b9048dc22fe80fe2ccbdbdc7b9e
nightmare9527 <101499016+nightmare9527@users.noreply.github.com>
2024-07-10 12:09:40 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="constyle.css">
+</head>
+<body>
+    <div class="mobile_button" id="mobile_button" >
+        <img src="image/burger-bar.png">
+    </div>
+    <header class="header" id="header">
+    <a href="home.php" class="logo">
+        健康共創協會
+    </a>
+    <nav class="navbar">
+            <ul class="choice">
+            <li><a href="home.php">首頁</a></li>
+            <li><a href="#">協會介紹</a>
+                <ul class="dropdown">
+                    <li><a href="#">章程</a></li>
+                    <li><a href="#">成立理念</a></li>
+                    <li><a href="#">加入我們</a></li>
+                </ul>
+            </li>
+            <li><a href="#">登入選項</a>
+                <ul class="dropdown">
+                    <li><a href="index2.php">創建帳號</a></li>
+                    <li><a href="index.php">登入</a></li>
+                </ul>
+            </li>
+            <li><a href="#">健康促進</a>
+                <ul class="dropdown">
+                    <li><a href="#">English</a></li>
+                    <li><a href="homeCH.php">繁體中文</a></li>
+                </ul>
+            </li>
+            <li><a href="#">醫療新知</a>
+                <ul class="dropdown">
+                    <li><a href="#">心理健康</a></li>
+                    <li><a href="homeCH.php">繁體中文</a></li>
+                </ul>
+            </li>
+            </ul>
+        </nav>
+        </header>
+<div class="container">
+    <h1 class="big-title">健康共創協會  章程:</h1>
+    <div class="guide">
+        <h2>第01條  本會名稱為健康共創協會（以下簡稱本會）。</h2>
+        <h2>第02條  本會為依法設立、非以營利為目的之公益性社會團體，以促進全人健康相關研究及知能之普及為宗旨。</h2>
+        <h2>第03條  本會之任務如下，與醫療專業人士或團體合作，並依相關法令規定推動及執行：</h2>
+        <!-- <p>
+            After logging in, you will reach the user page. Here, you will find four main options:
+        </p> -->
+        <ul>
+            <li>
+                <strong>一、</strong>支持健康及醫學研究。
+            </li>
+            <li>
+                <strong>二、</strong>促進專業人員跨領域醫學新知之交流。
+            </li>
+            <li>
+                <strong>三、</strong>推廣、支持與實踐實證醫學之全人發展及身心健康促進活動。
+            </li>
+            <li>
+                <strong>四、</strong>舉辦促進各年齡層全人身心健康之相關課程。
+            </li>
+            <li>
+                <strong>五、</strong>接受公、私部門機關或團體委託舉辦全人健康促進活動、研討、專案計劃。
+
+            </li>
+            <li>
+                <strong>六、</strong>提供有關生理、心理及社會福利之訊息及相關資源。
+            </li>
+            <li>
+                <strong>七、</strong>關懷身心障礙者之全齡身心發展以及照顧者生活品質提升。
+
+            </li>
+        </ul>
+        <h2>第04條  本會之主管機關為內政部，本會之目的事業應受各該事業主管機關之指導、監督。</h2>
+        <h2>第05條  本會以全國行政區域為組織區域。</h2>
+        <h2>第06條  本會會址設於主管機關所轄地區，並得報經主管機關核准設分支機構。
+              前項分支機構組織簡則由理事會擬訂，報請主管機關核准後行之。
+              會址及分支機構之地址於設置及變更時，應函報主管機關核備。</h2>
+        <h2>第07條  本會會員及會費分類如下：</h2>
+        <ul>
+            <li>
+                <strong>一、</strong>個人會員：凡贊同本會宗旨、具有行為能力者資格，填具入會申請書，經理事會審查通過，並繳納會費後，為個人會員；
+                入會費新臺幣100元，於會員入會時繳納；常年會 費1,000元。
+            </li>
+            <li>
+                <strong>二、</strong>贊助會員：凡贊同本會宗旨且贊助本會經費、資源之個人 或團體，填具入會申請書，經理事會審查通過後，為贊助會員。
+
+            </li>
+        </ul>
+        <!-- <p>
+            Remember to log out after using the website to ensure the security of your account.
+        </p> -->
+        <h2>第08條  會員（會員代表）有表決權、選舉權、被選舉權與罷免權。 每一會員（會員代表）為一權。贊助會員、榮譽會員無前項權利。</h2>
+        <h2>第09條  本會理事及監事，任期四年。</h2>
+        <h2>第10條  本會置理事9人（含常務理事3人，其中1人為理事長）、候補理事1人。常務理事，由全體理事互選之。理事長，由全體理事就常務理事中選舉之。</h2>
+        <h2>第11條  本會置監事3人（其中1人為常務監事）、候補監事1人。監事會置常務監事，由監事互選之，監察日常會務，並擔任監事會主席。</h2>
+    </div>
+</div>
+<script>
+        var buttonnav = document.getElementById("mobile_button");
+        var is_open = false;
+        buttonnav.addEventListener("click", function() {
+            var header = document.getElementById("header");
+            if (is_open) {
+                header.style.display = "none";
+                is_open = false;
+            } else {
+                header.style.display = "flex";
+                is_open = true;
+            }
+        });
+    </script>
+</body>
+</html>
